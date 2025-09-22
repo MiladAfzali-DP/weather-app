2008dc0aca643eb09965b93a5420dc2263c66ff0
Milad Afzali <miladsteam455@gmail.com>
2025-09-22 17:23:01 +0330

    coding DailyForecast Component

---
+++ src/components/DailyForecast/DailyForecast.jsx
@@ -0,0 +1,9 @@
+import "./DailyForecast.css";
+function DailyForecast() {
+  const dfDatas = [
+    { day: "Tue", icon: "rain", LowestTemp: "14˚", HighestTemp: "14˚" },
+  ];
+  return <div className="df"></div>;
+}
+
+export default DailyForecast;
