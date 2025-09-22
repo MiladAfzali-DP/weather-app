06a635e1154b8d2e529117c345e94bd42b422d28
Milad Afzali <miladsteam455@gmail.com>
2025-09-22 11:43:15 +0330

    coding Temperature component

---
+++ src/components/Temperature/Temperature.jsx
@@ -0,0 +1,17 @@
+import "./Temperature.css";
+function Temperature() {
+  return (
+    <div className="temp">
+      <div className="temp__left">
+        <h4>Berlin, Germany</h4>
+        <p>Tuesday,Aug 5, 2025</p>
+      </div>
+      <div className="temp__right">
+        <h3>20˚</h3>
+        <img src="/src/assets/images/icon-sunny.webp" alt="Kir" />
+      </div>
+    </div>
+  );
+}
+
+export default Temperature;
