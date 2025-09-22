101ed0dd4bb2b91998f24143b23cd6fbf4e6dfbc
Milad Afzali <miladsteam455@gmail.com>
2025-09-22 11:28:09 +0330

    coding Wraper Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import Container from "../Container/Container";
 import CitySearch from "../CitySearch/CitySearch";
 import Search from "../Search/Search";
 import Main from "../Main/Main";
+import Wraper from "../Wraper/Wraper";
 
 function App() {
   return (
@@ -24,7 +25,10 @@ function App() {
           <h1>How's the sky looking today?</h1>
           <Search />
         </CitySearch>
-        <Main></Main>
+        <Main>
+          <Wraper></Wraper>
+          <Wraper></Wraper>
+        </Main>
       </Container>
     </div>
   );
---
+++ src/components/Wraper/Wraper.jsx
@@ -0,0 +1,6 @@
+import "./Wraper.css";
+function Wraper({ children }) {
+  return <div className="wraper">{children}</div>;
+}
+
+export default Wraper;
