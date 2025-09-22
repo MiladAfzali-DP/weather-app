9234348e9a1c7b78d30ed806f11abe13bf6e9d02
Milad Afzali <miladsteam455@gmail.com>
2025-09-22 17:03:08 +0330

    add Default Style for Box Component

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,5 +1,16 @@
 function Box({ children, className }) {
-  return <div className={className}>{children}</div>;
+  const style = {
+    backgroundColor: "var(--neutral-700)",
+    borderRadius: "0.5rem",
+    width: "100%",
+    height: "5.5rem",
+    border: "1px solid var(--neutral-600)",
+  };
+  return (
+    <div style={style} className={className}>
+      {children}
+    </div>
+  );
 }
 
 export default Box;
